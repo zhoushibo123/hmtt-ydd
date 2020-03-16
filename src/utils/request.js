674ff545986a88bd47f32ceb1d16f49121ac11cd
zhoushibo <1216147314@qq.com>
2020-03-16 21:48:43 +0800

    处理token失效1

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,6 +7,7 @@
 import axios from 'axios' // 引入axios
 import JSONBig from 'json-bigint'// 引入大数字处理包
 import store from '../store'// 引入vuex中的实例对象store  相当于组件中的this.$store
+import router from '@/router'// 引入router对象
 const instance = axios.create({
 // 构造参数传入一些配置 和defaults一样
   baseURL: 'http://ttapi.research.itcast.cn/app/v1_0/', // 基础的请求地址
@@ -44,6 +45,33 @@ instance.interceptors.response.use(function (response) {
   }
 },
 function (error) {
+  // 如果请求失败或者失效或者其他错误 会进入到响应拦截器的错误区域
+  // 只要是401就是token失效（因为用导航守卫把没有token的拦截在外边了）
+  /*
+ error
+     config 是出像这次请求的配置信息
+     request请求对象
+     response响应对象 status（状态码）
+ */
+
+  if (error.response && error.response.status === 401) {
+    // 只要是401就是token失效
+    // refresh_token相当于续命的药
+    if (store.state.user.refresh_token) {
+      // 表示你有续命药
+    } else {
+      // 如果没有refresh_token 直接回到登录页 获取token
+
+      router.push({
+        path: '/login',
+        query: {
+          // 动态路由传参？ query传参
+          // 需要传递的query参数
+          redirectUrl: router.currentRoute.fullPath // 表示登录页需要跳转的地址
+        }
+      })
+    }
+  }
   return Promise.reject(error)
 }
 )
