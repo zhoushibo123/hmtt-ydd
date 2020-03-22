1ec88ba697b2db6420f50edcce06072f8599b5bb
zhoushibo <1216147314@qq.com>
2020-03-22 16:49:04 +0800

    搜索模块联想和历史记录显示和隐藏

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,16 +3,17 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <!-- 搜索组件 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
+    <!-- 搜索组件双向绑定 -->
+    <!-- 有输入内容的时候显示联想和没有输入内容的时候显示历史记录 -->
+    <van-search v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想内容 -->
-    <van-cell-group class="suggest-box" >
+    <van-cell-group v-if="q" class="suggest-box" >
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
-    <div class="history-box">
+    <div v-else class="history-box">
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -29,7 +30,12 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: ''// 关键字的数据
+    }
+  }
 }
 </script>
 
