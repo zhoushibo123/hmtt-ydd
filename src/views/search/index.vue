73096600978beef711818e4c7992ae3938691d2f
zhoushibo <1216147314@qq.com>
2020-03-23 18:25:16 +0800

    删除历史记录方法

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -14,14 +14,18 @@
     </van-cell-group>
     <!-- 历史记录 -->
     <div v-else class="history-box">
-      <div class="head">
+      <!-- 只有当历史纪录存在时才显示 历史记录 -->
+      <div class="head" v-if="historyList.length">
         <span>历史记录</span>
+
         <van-icon name="delete"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        <!-- 数据变成动态的 -->
+        <van-cell v-for="(item,index) in historyList" :key="index">
+          <a class="word_btn">{{item}}</a>
+           <!-- 给叉号注册点击事件 需要传对应的索引-->
+          <van-icon  @click="delHistory(index)" class="close_btn" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -29,12 +33,28 @@
 </template>
 
 <script>
+// 定义一个key,用来存储历史记录到本地缓存
+const key = 'hm-94-toutiao-history' // 此key用来作为 历史记录在本地缓存中的key
 export default {
   name: 'search',
   data () {
     return {
-      q: ''// 关键字的数据
+      q: '', // 关键字的数据
+      historyList: []// 作为一个变量 接受历史记录
     }
+  },
+  methods: {
+
+    delHistory (index) {
+      // 删除历史记录的方法
+      this.historyList = this.historyList.splice(index, 1)
+      // 删除后存到本地缓存
+      localStorage.setItem(key, JSON.stringify(this.historyList))
+    }
+  },
+  created () {
+    // 在钩子函数中读取历史数据
+    this.historyList = JSON.parse(localStorage.getItem(key) || '[]')
   }
 }
 </script>
