74d9d3b344d1be37310cd26df53b7724e13533a6
zhoushibo <1216147314@qq.com>
2020-03-24 22:37:04 +0800

    回复静态组件

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -23,7 +23,8 @@
           <p>{{ comment.content }}</p>
           <p>
             <span class="time">{{ comment.pubdate | relTime }}</span>&nbsp;
-            <van-tag plain @click="showReply=true">{{ comment.reply_count }}回复</van-tag>
+            <!-- 点击回复标签 弹出面板  还要处理许多业务-->
+            <van-tag plain @click="openReply()">{{comment.reply_count}} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -36,6 +37,19 @@
         <span class="submit" v-else slot="button">提交</span>
       </van-field>
     </div>
+    <!-- 放置回复面板 -->
+      <van-action-sheet v-model="showReply" :round="false" class="reply_dialog" title="回复评论">
+      <van-list v-model="reply.loading" :finished="reply.finished" finished-text="没有更多了">
+        <div class="item van-hairline--bottom van-hairline--top" v-for="index in 8" :key="index">
+          <van-image round width="1rem" height="1rem" fit="fill" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <div class="info">
+            <p><span class="name">一阵清风</span></p>
+            <p>评论的内容，。。。。</p>
+            <p><span class="time">两天内</span></p>
+          </div>
+        </div>
+      </van-list>
+    </van-action-sheet>
   </div>
 
 </template>
@@ -55,10 +69,22 @@ export default {
       // 控制提交中状态数据
       submiting: false,
       comments: [], // 接收评论
-      offset: null// 表示分页依据 如果为空,表示从第一页开始
+      offset: null, // 表示分页依据 如果为空,表示从第一页开始
+      showReply: false, // 控制评论的评论面板是否显示
+      reply: {
+        // 此对象专门放置 面板加载信息
+        loading: false, // 评论的评论的加载状态
+        finished: false, // 评论的评论是否加载完毕
+        offset: null, // 偏移量 作为评论的评论分页加载的时候 查询的依据
+        list: [] // 存放 评论的评论的数据
+      }
     }
   },
   methods: {
+    openReply () {
+      //   打开回复面板
+      this.showReply = true
+    },
     async  onLoad () {
       //   加载方法 滚动条距离底部超过一定距离就会执行
       const { artId } = this.$route.query// 拿到对应的参数 任何组件的属性中都有一个$route选项
@@ -86,6 +112,25 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.reply_dialog {
+  height: 100%;
+  max-height: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  .van-action-sheet__header {
+    background: #3296fa;
+    color: #fff;
+    .van-icon-close {
+      color: #fff;
+    }
+  }
+  .van-action-sheet__content{
+    flex: 1;
+    overflow-y: auto;
+    padding: 0 10px 44px;
+  }
+}
 .comment {
   margin-top: 10px;
   /deep/ .item {
